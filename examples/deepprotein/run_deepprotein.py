7f528af2a4c5cd917b4df62270e2d7986bfbcb4b
smiles724 <zh3971@163.com>
2025-10-12 21:26:18 -0700

    new model/data

--- examples/deepprotein/run_deepprotein.py
+++ examples/deepprotein/run_deepprotein.py
@@ -6,43 +6,28 @@ from rllm.agents.protein_agent import ProteinAgent
 from rllm.data.dataset import DatasetRegistry
 from rllm.engine.agent_execution_engine import AgentExecutionEngine
 from rllm.environments.base.single_turn_env import SingleTurnEnvironment
-from rllm.rewards.reward_fn import math_reward_fn
+from rllm.rewards.reward_fn import protein_reward_fn
 from rllm.utils import compute_pass_at_k
 
 if __name__ == "__main__":
     import os
+
     os.environ["TOKENIZERS_PARALLELISM"] = "true"
     n_parallel_agents = 64
     model_name = "agentica-org/DeepScaleR-1.5B-Preview"
+    dataset_name = " "
     tokenizer = AutoTokenizer.from_pretrained(model_name)
-    reward_fn = math_reward_fn
-    env_args = {
-        "reward_fn": reward_fn,
-    }
+    env_args = {"reward_fn": protein_reward_fn, }
     sampling_params = {"temperature": 0.6, "top_p": 0.95, "model": model_name}
 
-    engine = AgentExecutionEngine(
-        agent_class=ProteinAgent,
-        env_class=SingleTurnEnvironment,
-        agent_args={},
-        env_args=env_args,
-        engine_name="openai",
-        tokenizer=tokenizer,
-        sampling_params=sampling_params,
-        rollout_engine_args={
-            "base_url": "http://localhost:30000/v1",
-            "api_key": "None",
-        },
-        max_response_length=32768,
-        max_prompt_length=2048,
-        n_parallel_agents=n_parallel_agents,
-    )
+    engine = AgentExecutionEngine(agent_class=ProteinAgent, env_class=SingleTurnEnvironment, agent_args={}, env_args=env_args, engine_name="openai", tokenizer=tokenizer,
+                                  sampling_params=sampling_params, rollout_engine_args={"base_url": "http://localhost:30000/v1", "api_key": "None", }, max_response_length=32768,
+                                  max_prompt_length=2048, n_parallel_agents=n_parallel_agents, )
 
-    test_dataset = DatasetRegistry.load_dataset("aime2024", "test")
+    test_dataset = DatasetRegistry.load_dataset(f"protein_test_{dataset_name}", "test")
     if test_dataset is None:
         print("Dataset not found, preparing dataset...")
         from prepare_math_data import prepare_math_data
-
         _, test_dataset = prepare_math_data()
 
     tasks = test_dataset.repeat(n=16)  # repeat to evaluate pass@k
